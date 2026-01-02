3e5ba5280c78dcd758487a2c880f45d8faec4fcb
Myles de Bastion <42947408+mylesdebastion@users.noreply.github.com>
2026-01-02 14:45:32 -0800

    Initial Commit

---
+++ pixelboop/pixelboopApp.swift
@@ -0,0 +1,17 @@
+//
+//  pixelboopApp.swift
+//  pixelboop
+//
+//  Created by dbi mac mini m4 on 1/2/26.
+//
+
+import SwiftUI
+
+@main
+struct pixelboopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
